a725d10a342d05352965317e365299cd8a37be73
John Granström <granstrom.john@gmail.com>
2017-02-08 13:13:38 +0100

    Ensure signals are passed to kairos

--- config-kairos.sh
+++ config-kairos.sh
@@ -10,4 +10,4 @@ fi
 #sed -i "s/^kairosdb.jetty.port.*$/kairosdb.jetty.port=$KAIROS_JETTY_PORT/" /opt/kairosdb/conf/kairosdb.properties
 sed -i "s/^kairosdb.datastore.cassandra.host_list.*$/kairosdb.datastore.cassandra.host_list=$CASSANDRA_HOST_LIST/" /opt/kairosdb/conf/kairosdb.properties
 
-/opt/kairosdb/bin/kairosdb.sh run
+exec /opt/kairosdb/bin/kairosdb.sh run
